fcd8cfd4d834ff93af3722a5116f5be6f1171db4
fakhri.afrasiyabov@ibar.az <fakhri.afrasiyabov@ibar.az>
2021-10-13 10:22:20 +0400

    easy algos

---
+++ src/easyAlgo/LeftRotation.java
@@ -0,0 +1,35 @@
+package easyAlgo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeftRotation {
+    public static List<Integer> rotLeft(int d, List<Integer> ar) {
+        int[] a = new int[ar.size()];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = ar.get(i).intValue();
+        }
+        int length = a.length;
+        List<Integer> temp = new ArrayList<>();
+        int j = 0;
+
+        for (int i = d; i < length; i++, j++) {
+            temp.add(j, a[i]);
+        }
+        for (int i = 0; i < d; i++, j++) {
+            temp.add(j, a[i]);
+        }
+        System.out.println(temp);
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> temp = new ArrayList<>();
+        temp.add(1);
+        temp.add(2);
+        temp.add(3);
+        temp.add(4);
+        temp.add(5);
+        rotLeft(2, temp);
+    }
+}
---
+++ src/easyAlgo/ReverseArray.java
@@ -0,0 +1,23 @@
+package easyAlgo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseArray {
+    public static List<Integer> reverseArray(List<Integer> a) {
+        List<Integer> reversed = new ArrayList<>();
+        for (int i = a.size() - 1; i >= 0; i--) {
+            reversed.add(a.get(i));
+        }
+        System.out.println(reversed);
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        reverseArray(list);
+    }
+}
